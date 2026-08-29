bc99ad05d9c48fff967f16eca3dc44aa815b6862
Vitthal Kadam <vitthalvkadam@gmail.com>
2026-08-29 14:03:05 +0530

    Time: 2 ms (89.08%), Space: 48.1 MB (8.59%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -2,6 +2,9 @@ class Solution {
     public boolean canJump(int[] nums) {
         int maxReach = 0;
 
+        // Keep the maximum position we can reach.
+        // If we cannot reach the current position, return false; otherwise, reach the end and return true.
+        
         for(int i = 0; i<nums.length; i++){
             if(i > maxReach){
                 return false;
